8cb65bb855ac836f99e3ac8c6c1c3699384f3bd3
linuxbytes <real8686@gmail.com>
2020-07-24 22:44:27 -0400

    add testing loop for data

--- client-api-call.js
+++ client-api-call.js
@@ -1,14 +1,27 @@
-
-// Basic client call for APU
+// Basic client call for API of github
+let jsondata;
 
 fetch('https://api.github.com/graphql', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization':''
-  },
-  body: JSON.stringify( { 'query': 
-  'query{ user(login: "linuxbytes"){repositories(first:100){nodes{primaryLanguage{name}}}}}'
-} )
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer dac9275d17f36ca1f442aaeb044c015f7c6a0d36'
+    }, 
+    body: JSON.stringify(
+        {'query': 'query{ user(login: "linuxbytes"){repositories(first:100){nodes{primaryLanguage{name}}}}}'}
+    )
+}).then(function (u) {
+    return u.json();
+}).then(function (json) {
+    jsondata = json;
+})
 
+
+console.log(jsondata);
+
+// for (key in jsondata.data.user.repositories.nodes){
+//   for (x in jsondata.data.user.repositories.nodes[key].primaryLanguage){
+//     console.log(jsondata.data.user.repositories.nodes[key].primaryLanguage[x]);
+//   }
+// }
