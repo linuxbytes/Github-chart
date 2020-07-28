a0f89b4c9e4e38aa755253addf62e747add2c240
linuxbytes <real8686@gmail.com>
2020-07-27 21:32:05 -0400

    clean up

--- client-api-call.js
+++ client-api-call.js
@@ -4,57 +4,64 @@
 
 // set the dimensions and margins of the graph
 var width = 650
-    height = 650
-    margin = 60
+height = 650
+margin = 60
 
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
 var radius = Math.min(width, height) / 2 - margin
 
 // append the svg object to the div called 'my_dataviz'
-var svg = d3.select("#github_chart")
-  .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+var svg = d3.select("#github_chart").append("svg").attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 var data_objects = {
-  "HTML": 11,
-  "Python": 18,
-  "Shell": 4,
-  "JavaScript": 11,
-  "CSS": 3,
-  "Go": 2,
-  "Vim script": 1,
-  "C": 2,
-  "Swift": 1,
-  "C++": 3,
-  "PowerShell": 1
+    "HTML": 11,
+    "Python": 18,
+    "Shell": 4,
+    "JavaScript": 11,
+    "CSS": 3,
+    "Go": 2,
+    "Vim script": 1,
+    "C": 2,
+    "Swift": 1,
+    "C++": 3,
+    "PowerShell": 1
 }
 // var data_objects = {a: 90, b: 10, c:30, d:8, e:12}
 
 // const data_objects = Object.values(jsondata.data.user.repositories.nodes);
-//set the color scale
-var color = d3.scaleOrdinal()
-  .domain(data_objects)
-  .range(["#FE9A2E", "#0080FF", "#6E6E6E", "#FFFF00", "#2E2EFE","#088A29","#1C1C1C","#FE642E","#D8D8D8","#8000FF"])
+// set the color scale
+var color = d3.scaleOrdinal().domain(data_objects)
+// "HTML": FE9A2E,
+// "Python": 0080FF,
+// "Shell": 6E6E6E,
+// "JavaScript": FFFF00,
+// "CSS": 2E2EFE,
+// "Go": 088A29,
+// "Vim script": 1C1C1C,
+// "C": FE642E,
+// "Swift": D8D8D8,
+// "C++": 8000FFƒ,
+// "PowerShell": 1
+.range([
+    "#FE9A2E",
+    "#0080FF",
+    "#6E6E6E",
+    "#FFFF00",
+    "#2E2EFE",
+    "#088A29",
+    "#1C1C1C",
+    "#FE642E",
+    "#D8D8D8",
+    "#8000FF"
+])
 
 // Compute the position of each group on the pie:
-var pie = d3.pie()
-  .value(function(d) {return d.value; })
+var pie = d3.pie().value(function (d) {
+    return d.value;
+})
 var data_ready = pie(d3.entries(data_objects))
 
 // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-svg
-  .selectAll('whatever')
-  .data(data_ready)
-  .enter()
-  .append('path')
-  .attr('d', d3.arc()
-    .innerRadius(0)
-    .outerRadius(radius)
-  )
-  .attr('fill', function(d){ return(color(d.data.key)) })
-  .attr("stroke", "black")
-  .style("stroke-width", "2px")
-  .style("opacity", 0.7)
+svg.selectAll('whatever').data(data_ready).enter().append('path').attr('d', d3.arc().innerRadius(0).outerRadius(radius)).attr('fill', function (d) {
+    return(color(d.data.key))
+}).attr("stroke", "black").style("stroke-width", "2px").style("opacity", 0.7)
